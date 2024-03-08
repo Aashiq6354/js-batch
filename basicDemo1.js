c2e4335d5d42d83865a56bfa5df81c9b0152a3ca
Aashiq singh <108392697+Aashiq6354@users.noreply.github.com>
2024-03-08 10:45:12 +0530

    Add files via upload
    Basic

---
+++ basicDemo1.js
@@ -0,0 +1,31 @@
+// console.log("hello world");
+// console.warn("Heelo this is warn function");
+// console.info("Infomation function")
+// console.error("Error Function");
+
+
+a = 10;
+
+console.log("Value of a = "+a);
+console.log("type of a = ",typeof(a));
+
+b = 20.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof b);
+
+c = "Hello World";
+console.log("Value of c =",c)
+console.log("Type of c=",typeof c)
+
+
+isActive = true 
+console.log("isActive = ",isActive);
+console.log("Type of isActive= ",typeof isActive)
+
+let x;
+console.log("value of x = ",x);
+console.log("Type of x = ", typeof x)
+
+
+
+// Datetypes :- number , string , Boolean ,  Null and Undefined.
