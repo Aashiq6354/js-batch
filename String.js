e026ed2bf6e4a2063f9ee33ed859dde257abe31a
Aashiq singh <108392697+Aashiq6354@users.noreply.github.com>
2024-03-27 10:46:01 +0530

    Add files via upload
    String

---
+++ String.js
@@ -0,0 +1,46 @@
+
+
+// console.log(a);
+// console.log(a.length);
+
+// var x = a.charAt(3) //returns the character at index 
+// console.log(x);
+
+// x = a.charCodeAt(3);
+// console.log(x);
+
+
+// x = a.concat(" is awesome");
+// console.log(x);
+
+// x = a.endsWith("t")
+// console.log(x);
+
+// x = a.includes("Z");
+// console.log(x);
+
+
+
+// x = a.lastIndexOf("o");
+// console.log(x);
+
+// var x = a.repeat(4);
+// console.log(x);
+
+x = a.replace("Royal","Shree");
+console.log(x);
+
+// console.log(a.replace("a","v"));
+
+
+var a = "Royal techanosofT";
+
+// var x = a.search("o")
+// console.log(x);
+
+// x = a.indexOf("o");
+// console.log(x);
+
+var x = a.slice(6 , 8)
+console.log(x);
+
